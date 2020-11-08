0e0071817235ecac5655dd0e2964b104b4c77ae2
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-11-08 19:10:36 -0300

    docs(conceptos): seprar uPort y DIDI

--- sidebars.js
+++ sidebars.js
@@ -2,11 +2,12 @@ module.exports = {
   someSidebar: {
     "Visión general": ["overview"],
     "Conceptos importantes": ['conceptos'],
+    Producto: ['producto-emisores'],
     "Contribuciones": ['contribuciones', 'code-fo-conduct'],
     Developers: [
+      { "Solución": ["DIDI", "uport"] },
       { "Troubleshooting": ["troubleshooting", "troubleshooting-rsk", 'revokation']}, 
       { "Multiblockchain": ['multiblockchain', 'multiblockchain-scripts']}
     ],
-      Producto: ['producto-emisores'],
   },
 };
