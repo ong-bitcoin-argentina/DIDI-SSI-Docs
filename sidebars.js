31762bc20d6253b07c442b57918bb3b5a456095a
Matias Oveja Smith <matias.ovejasmith@bitcoinargentina.org>
2021-06-23 18:56:00 -0300

    Add changelog section

--- sidebars.js
+++ sidebars.js
@@ -32,5 +32,7 @@ module.exports = {
       { "Troubleshooting": ['developers/troubleshooting/troubleshooting', 'developers/troubleshooting/troubleshooting-rsk', 'developers/troubleshooting/revocation',] },
       { "Multiblockchain": ['developers/multiblockchain/multiblockchain', 'developers/multiblockchain/multiblockchain-scripts',] }
     ],
+
+    "Changelog": ['changelog',],
   },
 };
